684be53f857c363b0d71ae66bddd76d32938d0b7
D43m0n <dortsen@gmail.com>
2015-12-10 13:49:20 +0100

    added check for lsb_release and basic 'case' skeleton for two distributions

--- deploy-puppet.sh
+++ deploy-puppet.sh
@@ -3,6 +3,25 @@
 # Adapted from https://puppetlabs.com/blog/bootstrap-rackspace-cloud-servers-puppet-and-libcloud
 # and https://www.digitalocean.com/community/tutorials/how-to-install-puppet-in-standalone-mode-on-centos-7
 
+# Check for required tool
+LSB=lsb_release
+type -P "${LSB}" > /dev/null && echo "${LSB} found, continuing..." || { echo "${LSB} not found, install first! Now exiting..."; exit 1; }
+
+# Determine distro
+DISTRO=$(lsb_release -si)
+
+case ${DISTRO} in
+    CentOS)
+        echo "CentOS detected"
+        ;;
+    Ubuntu)
+        echo "Ubuntu detected"
+        ;;
+    *)
+        echo "Unsupported distribution. Sorry..."
+        exit 2
+esac
+
 # First, upgrade packages
 yum -y update
 
